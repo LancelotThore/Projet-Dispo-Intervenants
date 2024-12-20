82ab75c505779e36cc3b828ba9596e0a370b845a
LancelotThore <144225580+LancelotThore@users.noreply.github.com>
2024-12-20 14:08:49 +0100

    fix

--- app/availability/[key]/page.tsx
+++ app/availability/[key]/page.tsx
@@ -2,6 +2,7 @@ import { fetchIntervenantByKey } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import Calendar from '@/app/ui/calendar';
 import { checkAvailabilityAndWorkweek } from '@/app/lib/actions';
+import { ExclamationTriangleIcon } from '@/app/ui/icons';
 
 const AvailabilityPage = async ({ params }: { params: { key: string } }) => {
   const key = params.key;
@@ -35,14 +36,23 @@ const AvailabilityPage = async ({ params }: { params: { key: string } }) => {
           </div>
         )}
         {insufficientHours.length > 0 && (
-          <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
-            <strong className="font-bold">Attention!</strong>
-            <span className="block sm:inline"> Vous avez saisi moins d'heures que nécessaire pour les semaines suivantes:</span>
-            <ul className="list-disc list-inside">
-              {insufficientHours.map(({ week, totalHours, requiredHours }) => (
-                <li key={week}>Semaine {week}: {totalHours} heures saisies, {requiredHours} heures requises</li>
-              ))}
-            </ul>
+          <div className="bg-red-50 border-l-4 border-red-400 text-red-700 p-4 mb-4 rounded-md shadow-md" role="alert">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <ExclamationTriangleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+              </div>
+              <div className="ml-3">
+                <h3 className="text-sm font-medium">Attention!</h3>
+                <div className="mt-2 text-sm">
+                  <p>Vous avez saisi moins d'heures que nécessaire pour les semaines suivantes:</p>
+                  <ul className="list-disc list-inside mt-2">
+                    {insufficientHours.map(({ week, totalHours, requiredHours }) => (
+                      <li key={week}>Semaine {week}: {totalHours} heures saisies, {requiredHours} heures requises</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         )}
         <Calendar availability={intervenant.availability ?? ''} intervenantKey={intervenant.key} />
--- app/dashboard/disponibility/page.tsx
+++ app/dashboard/disponibility/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { fetchIntervenantsKey } from '@/app/lib/data';
 import { checkAvailabilityAndWorkweek } from '@/app/lib/actions';
+import { ExclamationTriangleIcon } from '@/app/ui/icons';
 import Calendar from '@/app/ui/calendar';
 
 export default function Disponibility() {
@@ -69,14 +70,23 @@ export default function Disponibility() {
             </div>
           )}
           {insufficientHours.length > 0 && (
-            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
-              <strong className="font-bold">Attention!</strong>
-              <span className="block sm:inline"> Vous avez saisi moins d'heures que nécessaire pour les semaines suivantes:</span>
-              <ul className="list-disc list-inside">
-                {insufficientHours.map(({ week, totalHours, requiredHours }) => (
-                  <li key={week}>Semaine {week}: {totalHours} heures saisies, {requiredHours} heures requises</li>
-                ))}
-              </ul>
+            <div className="bg-red-50 border-l-4 border-red-400 text-red-700 p-4 mb-4 rounded-md shadow-md" role="alert">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <ExclamationTriangleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+                </div>
+                <div className="ml-3">
+                  <h3 className="text-sm font-medium">Attention!</h3>
+                  <div className="mt-2 text-sm">
+                    <p>Vous avez saisi moins d'heures que nécessaire pour les semaines suivantes:</p>
+                    <ul className="list-disc list-inside mt-2">
+                      {insufficientHours.map(({ week, totalHours, requiredHours }) => (
+                        <li key={week}>Semaine {week}: {totalHours} heures saisies, {requiredHours} heures requises</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
           <Calendar key={selectedIntervenant.key} availability={selectedIntervenant.availability ?? ''} intervenantKey={selectedIntervenant.key} />
